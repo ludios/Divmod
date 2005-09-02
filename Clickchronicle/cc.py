ce03b626068a2a4c76a83b08cf64a0d5a935dc38
amir <amir@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-09-02 14:36:49 +0000

    initial commit of clickchronicle files
    git-svn-id: http://divmod.org/svn/Divmod/trunk@181 866e43f7-fbfc-0310-8f2a-ec88d1da2979

---
+++ Clickchronicle/cc.py
@@ -0,0 +1,61 @@
+from axiom.item import Item
+from axiom import attributes
+from zope.interface import implements
+from xmantissa.webnav import Tab
+from xmantissa.ixmantissa import INavigableElement, IBenefactor
+from xmantissa.webadmin import ParentCounterMixin
+from nevow import loaders, rend, livepage
+from twisted.python.components import registerAdapter
+from xmantissa.webadmin import WebSite, PrivateApplication
+
+#class ClickChronicleApplication( PrivateApplication ):
+    #implements( INavigableElement )
+    #typeName = 'clickchronicle_application'
+    #schemaVersion = 1
+    #urlCount = attributes.integer( default = 0 )
+
+    #def install( self ):
+    #    self.store.powerUp( self, INavigableElement )
+
+    #def getTabs( self ):
+    #    return [Tab('Admin', self.storeID, 0.0,
+    #                [Tab('Magical', self.storeID, 0.1)])]
+
+    #def createResource( self, a, b, c, d, e, f, g ):
+    #    pass
+#    pass
+
+class Visit( Item ):
+    timestamp = attributes.timestamp
+    url = attributes.text
+
+    schemaVersion = 1
+    typeName = 'visit'
+
+from nevow.inevow import IResource, IQ
+
+class LinkList( rend.Page ):
+    implements( IResource )
+    docFactory = loaders.xmlfile( 'linklist.xml' )
+
+    def __init__( self, *a, **k ):
+        rend.Page.__init__( self, *a, **k )
+        self.store = self.original.store
+
+    def data_links( self, ctx, data ):
+        for visit in self.store.query( Visit ):
+            yield dict( timestamp = visit.timestamp, url = visit.url )
+        
+#registerAdapter( LinkList, ClickChronicleApplication, IResource )
+
+class ClickChronicleBenefactor( Item ):
+    implements( IBenefactor )
+    typeName = 'clickchronicle_benefactor'
+    schemaVersion = 1
+
+    endowed = attributes.integer( default = 0 )
+
+    def endow(self, ticket, avatar):
+        self.endowed += 1
+        for item in ( WebSite, PrivateApplication ):
+            item( store = avatar ).install()

b3864716565d1231df1195368d2e34b697bc6f1c
moe <moe@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-09-06 16:21:07 +0000

    comments?  comments!
    git-svn-id: http://divmod.org/svn/Divmod/trunk@192 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/cc.py
+++ ClickChronicle/clickchronicle/cc.py
@@ -12,14 +12,18 @@ from epsilon.extime import Time
 from datetime import datetime
 
 class Visit( Item ):
+    '''i correspond to a webpage-visit logged by a clickchronicle user'''
     timestamp = attributes.timestamp()
-    url = attributes.bytes()
+    url = attributes.bytes() 
     title = attributes.bytes()
 
     schemaVersion = 1
     typeName = 'visit'
 
 class Preferences( Item ):
+    '''i represent storeable, per-user preference information.
+       i implement INavigableElement, so PrivateApplication will
+       look for me in the user's store'''
     implements( ixmantissa.INavigableElement )
     typeName = 'clickchronicle_preferences'
     schemaVersion = 1
@@ -31,9 +35,12 @@ class Preferences( Item ):
         self.store.powerUp( self, ixmantissa.INavigableElement )
 
     def getTabs( self ):
-        return [Tab('Preferences', self.storeID, 0.0)]
+        return [Tab('Preferences', self.storeID, 0.2)]
 
 class PreferencesFragment( rend.Fragment ):
+    '''i will get an adapter for Preferences instances, who
+       implements INavigableFragment'''
+       
     fragmentName = 'preferences-fragment'
     title = ''
     live = True
@@ -42,6 +49,7 @@ class PreferencesFragment( rend.Fragment ):
         return ''
 
     def data_preferences( self, ctx, data ):
+        '''return a dict of self.original's (Preferences instance) columns'''
         return dict( displayName = self.original.displayName,
                      homepage = self.original.homepage )
 
@@ -50,18 +58,24 @@ registerAdapter( PreferencesFragment,
                  ixmantissa.INavigableFragment )
 
 class LinkList( Item ):
+    '''similar to Preferences, i am an implementor of INavigableElement,
+       and PrivateApplication will find me when when it looks in the user's
+       store'''
+       
     implements( ixmantissa.INavigableElement )
     typeName = 'clickchronicle_linklist'
-    attribute = attributes.integer( default = 0 ) # ?
+    links = attributes.integer( default = 0 )
     schemaVersion = 1
 
     def install( self ):
         self.store.powerUp( self, ixmantissa.INavigableElement )
 
     def getTabs( self ):
-        return [Tab('My Clicks', self.storeID, 0.0)]
+        '''show a link to myself in the navbar'''
+        return [Tab('My Clicks', self.storeID, 0.1)]
 
 class LinkListFragment( rend.Fragment ):
+    '''i adapt LinkList to INavigableFragment'''
     fragmentName = 'link-list-fragment'
     title = ''
     live = True
@@ -70,6 +84,8 @@ class LinkListFragment( rend.Fragment ):
         return ''
 
     def data_links( self, ctx, data ):
+        '''find all Visits in the user's store, sort them by timestamp
+           and yield them to the template'''
         store = self.original.store
         for visit in store.query( Visit, sort = Visit.timestamp.descending ):
             yield dict( url = visit.url, 
@@ -81,10 +97,13 @@ registerAdapter( LinkListFragment,
                  ixmantissa.INavigableFragment )
 
 class URLGrabber( rend.Page ):
+    '''i handle ClickRecorder's HTTP action.  i am not an Item
+       because i have a lot of attributes inherited from rend.Page'''
     def __init__( self, recorder ):
         self.recorder = recorder
         
     def renderHTTP( self, ctx ):
+        '''get url and title GET variables, supplying sane defaults'''
         urlpath = inevow.IRequest( ctx ).URLPath()
         qargs = dict( urlpath.queryList() )
         url = qargs.get( 'url' )
@@ -94,9 +113,16 @@ class URLGrabber( rend.Page ):
                   url = url,
                   timestamp = Time.fromDatetime( datetime.now() ),
                   title = qargs.get( 'title', 'Untitled' ))
+            
+        (linkList,) = list(self.recorder.store.query(LinkList))
+        linkList.links += 1
+
         return ''
             
 class ClickRecorder( Item, PrefixURLMixin ):
+    '''i exist independently of the rest of the application
+       and accept HTTP requests at private/record, which i
+       farm off to URLGrabber'''
     schemaVersion = 1
     implements( ixmantissa.ISiteRootPlugin )
     typeName = 'clickrecorder'
@@ -110,6 +136,9 @@ class ClickRecorder( Item, PrefixURLMixin ):
         return URLGrabber( self )
 
 class ClickChronicleBenefactor( Item ):
+    '''i am responsible for granting priveleges to avatars, 
+       which equates to installing stuff in their store'''
+       
     implements( ixmantissa.IBenefactor )
     typeName = 'clickchronicle_benefactor'
     schemaVersion = 1

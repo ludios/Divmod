2575634e5450b08398608109f3b623b7128a8088
amir <amir@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-09-18 00:08:37 +0000

    fix
    git-svn-id: http://divmod.org/svn/Divmod/trunk@275 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/stats/bigram.py
+++ ClickChronicle/clickchronicle/stats/bigram.py
@@ -37,6 +37,8 @@ def buildMutualInfo(count, unigrams, bigrams):
         sProb=log(sCount/count)
         pairProb=(pairCount/count)
         mi=pairProb - fProb + sProb
+        mi=log(count*pairCount/(fCount*sCount))/log(2.0)
+        #log(N*$1/(f[$2]*f[$3]))/log(2.0), f[$2], f[$3], $0)}
         mutualInfo.append((mi, fCount, sCount, pairCount, first, second))
     mutualInfo.sort(lambda a, b: cmp(b[0], a[0]))
     return mutualInfo
@@ -45,7 +47,7 @@ def log_l(p, total, k, cols=2, m=4):
     fSum = 0.0
     logTotal = log(total)
 
-    for j in range(0,2):
+    for j in range(cols):
         if p[j]>0:
             fSum += k[j] * log(p[j]/total)
         elif k[j]>0:
@@ -56,42 +58,74 @@ def likelihoodRatio(count, mi):
     mi, fCount, sCount, pairCount, first, second = mi
     cTable = {}
     cTable[(0,0)]=pairCount
-    cTable[(0,1)]=fCount - pairCount
-    cTable[(1,0)]=sCount - pairCount
+    cTable[(0,1)]=sCount - pairCount
+    cTable[(1,0)]=fCount - pairCount
     cTable[(1,1)]=count-fCount-sCount+pairCount
 
     rowSum = [0.0, 0.0]
     colSum = [0.0, 0.0]
     total = 0.0
     
-    for i in range(0,2):
+    for i in range(2):
         rowSum[i]=0.0
-        for j in range(0,2):
+        for j in range(2):
             rowSum[i]+=cTable[(i,j)]
             colSum[j]+=cTable[(i,j)]
         total+=rowSum[i]
 
     fSum = 0.0
     for i in range(0,2):
-        fSum += (log_l([cTable[(0,0)], cTable[(i,1)]],rowSum[i],
-                       [cTable[(0,0)], cTable[(i,1)]]) -
-                log_l(colSum, total, [cTable[(0,0)], cTable[(i,1)]]))
-    return fSum * 2
+        fSum += (log_l([cTable[(i,0)], cTable[(i,1)]],rowSum[i],
+                       [cTable[(i,0)], cTable[(i,1)]]) -
+                log_l(colSum, total, [cTable[(i,0)], cTable[(i,1)]]))
+    return fSum * 2.0
         
 
 if __name__ == '__main__':
-    import sys
-    str = """speno: The funny thing is, I actually had typed those exact
-    words above the cover image, but I took them out before posting to
-    let people draw their own conclusions. So now I can say it: I
-    agree, it truly is the best cover ever. The O.Reilly designers
-    nailed it"""
-    count, unigrams, bigrams= buildCounts(str)
-    res = buildMutualInfo(count, unigrams, bigrams)
-    final = [likelihoodRatio(count, r) for r in res]
+    from clickchronicle.stats.stoplist import stopwords
+    if False:
+        str = """speno: The funny thing is, I actually had typed those exact
+        words above the cover image, but I took them out before posting to
+        let people draw their own conclusions. So now I can say it: I
+        agree, it truly is the best cover ever. The O.Reilly designers
+        nailed it"""
+        count, unigrams, bigrams= buildCounts(str)
+        res = buildMutualInfo(count, unigrams, bigrams)
+        final = [likelihoodRatio(count, r) for r in res]
     
-    for pair in zip(final, res):
-        print pair
+        zipped = zip(final, res)
+        zipped.sort(lambda a,b: cmp(b[0], a[0]))
+        for pair in zipped:
+            print pair
+    
+    import sys
+    from clickchronicle import tagstrip
+    fnames = sys.argv[1:]
+
+    for fname in fnames:
+        print '---'
+        print fname, 
+        source = open(fname, 'rb').read()
+        #(text, meta) = tagstrip.cook(source)
+        text = source
+        
+        count, unigrams, bigrams= buildCounts(text)
+        print count
+        count -= 1
+        res = buildMutualInfo(count, unigrams, bigrams)
+        #for r in res:
+        #    print r
+        final = [likelihoodRatio(count, r) for r in res]
+
+        zipped = zip(final, res)
+        zipped.sort(lambda a,b: cmp(b[0], a[0]))
+        for score, data in zipped:
+            mi, fCount, sCount, pairCount, first, second = data
+            if first not in stopwords and second not in stopwords:
+                print score, data
+            
+            
+    
     
 
         

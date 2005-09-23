078c8b9c9e47ba65a6f9af379c1ff6decb18c1bb
amir <amir@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-09-23 17:12:46 +0000

    at least index unicode correctly even if we cant search it correclty yet
    git-svn-id: http://divmod.org/svn/Divmod/trunk@331 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/indexinghelp.py
+++ ClickChronicle/clickchronicle/indexinghelp.py
@@ -12,11 +12,11 @@ class SyncIndexer(Item):
     """
     Implements a synchronous in-process full-text indexer.
     """
-    
+
     schemaVersion = 1
     typeName = 'syncindexer'
     indexCount = attributes.integer(default=0)
-    
+
     implements(IIndexer)
 
     def installOn(self, other):
@@ -26,13 +26,13 @@ class SyncIndexer(Item):
         def txn():
             self.indexCount = newCount
         self.store.transact(txn)
-        
+
     def incrementIndexCount(self):
         self._setIndexCount(self.indexCount + 1)
 
     def decrementIndexCount(self):
         self._setIndexCount(self.indexCount - 1)
-        
+
     def index(self, item):
         def cbIndex(doc):
             self.incrementIndexCount()
@@ -70,13 +70,24 @@ class SyncIndexer(Item):
         count = xapIndex.count(query)
         xapIndex.close()
         return count
-        
+
 from twisted.web import client
 def getPageSource(url):
     """Asynchronously get the page source for a URL.
     """
-        
-    return client.getPage(url)    
+    return client.getPage(url)
+
+def getContentType(meta):
+    if 'http-equiv' in meta:
+        equivs=meta['http-equiv']
+        if 'content-type' in equivs:
+            ctype = equivs['content-type']
+            # ctype should be something like this 'text/html; charset=iso-8859-1'
+            type, enc = ctype.split(';',1)
+            enc = enc.strip()
+            _, enc = enc.split('=',1)
+            return enc
+    return None
 
 def makeDocument(visit, pageSource):
     values = [
@@ -84,23 +95,32 @@ def makeDocument(visit, pageSource):
         Value('url', visit.url),
         Value('title', visit.title)]
     (text, meta) = tagstrip.cook(pageSource)
+    title = visit.title
+    encoding = getContentType(meta)
+    if encoding is not None:
+        text = text.decode(encoding)
+        title = title.decode(encoding)
     terms = []
-    if meta:
-        sa = StandardAnalyzer()
-        for contents in meta.itervalues():
-            for value in contents:
-                for tok in sa.tokenize(value):
-                    terms.append(Term(tok))
+    #if meta:
+    #    sa = StandardAnalyzer()
+    #    for contents in meta.itervalues():
+    #        for value in contents:
+    #            for tok in sa.tokenize(value):
+    #                terms.append(Term(tok))
+
     # Add page text
     textFields = [TextField(text)]
-    # Use storeID for possibly simpler removal of visit from index at a later stage
+    if visit.title:
+        textFields.append(TextField(visit.title))
+    # Use storeID for simpler removal of visit from index at a later stage
     doc = Document(uid=visit.storeID,
                    textFields=textFields,
                    values=values,
                    terms=terms,
                    source=pageSource)
     return doc
-    
+
+
 if __name__ == '__main__':
     import sys
     fnames = sys.argv[1:]
@@ -111,5 +131,3 @@ if __name__ == '__main__':
         print meta
         print '***********'
         print text
-        
-        
--- ClickChronicle/clickchronicle/tagstrip.py
+++ ClickChronicle/clickchronicle/tagstrip.py
@@ -31,9 +31,19 @@ class OxTail(Minestrone):
 
     def do_meta(self, attrs):
         attrs = dict(attrs)
-        if 'name' in attrs and 'content' in attrs:
-            values = self.metaTags.setdefault(attrs['name'], [])
-            values.append(attrs['content'])
+        # XXX - Not pretty! Depends on there only being 2 keys in attrs
+        # one of which is content. Returns dict of dicts. First dict key
+        # is the word immediately after META e.g. 'http-equiv' or name.
+        # The key of the nested dict is the value of the first key and
+        # the value at the nested dict is the content.
+        if 'content' in attrs:
+            content = attrs.pop('content')
+            key, value = attrs.popitem()
+            thisDict = self.metaTags.setdefault(key, {})
+            thisDict[value]=content
+#        if 'name' in attrs and 'content' in attrs:
+#            values = self.metaTags.setdefault(attrs['name'], [])
+#            values.append(attrs['content'])
 
     def handle_data(self, data):
         if not data.isspace():

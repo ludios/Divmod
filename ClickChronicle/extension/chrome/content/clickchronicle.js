6a98fc9cb77364c51e0f887d57a560e725487e7d
moe <moe@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-09-27 15:54:25 +0000

    operational extension
    git-svn-id: http://divmod.org/svn/Divmod/trunk@354 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/extension/chrome/content/clickchronicle.js
+++ ClickChronicle/extension/chrome/content/clickchronicle.js
@@ -4,14 +4,43 @@ var gIOSvc = Components.classes["@mozilla.org/network/io-service;1"]
 var gConsoleSvc = Components.classes['@mozilla.org/consoleservice;1']
                     .getService(Components.interfaces.nsIConsoleService);
                     
-function recordableURI(uri) { return true; }
+var gCCPrefs = Components.classes["@mozilla.org/preferences-service;1"]
+                .getService(Components.interfaces.nsIPrefService)
+                    .getBranch("extensions.ClickChronicle.");
+
+function substitute() {
+    var args = substitute.arguments;
+    var str  = args[0];
+    for( i = 1 ; i < args.length ; i++ )
+        str = str.replace( /%s/, args[i] );
+    return str;
+}
+        
+function recordableURI( URI ) {
+    /* optimize preference getting */
+    var gRecorderURI = gIOSvc.newURI(gCCPrefs.getCharPref("clickRecorderURL"), null, null);
+    return URI.schemeIs("http") && (URI.userPass == "") && (URI.host != gRecorderURI.host)
+}
+
+function logToServer(document) {
+    gConsoleSvc.logStringMessage("recording: " + document.location.href + " " + document.title);
+    var req = new XMLHttpRequest();
+    var targetURL = gCCPrefs.getCharPref("clickRecorderURL");
+    targetURL += substitute("?url=%s&title=%s&ref=%s",
+                            encodeURIComponent(document.location.href),
+                            encodeURIComponent(document.title),
+                            encodeURIComponent(document.referrer));
+                            
+    req.open("POST", targetURL, true);
+    req.send(null);
+}
 
 function domContentLoaded(tabBrowser, appContent, event) {
     var win = event.target.defaultView;
     if(win == win.top) {
         var URI = gIOSvc.newURI(win.location.href, null, null);
-        if(recordableURI(URI))
-            gConsoleSvc.logStringMessage(win.location.href + " " + win.document.title);
+        if(recordableURI(URI)) 
+            logToServer(win.document);
     }
 }
 

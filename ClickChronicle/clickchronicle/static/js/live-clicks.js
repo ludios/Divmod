0282e854c01837261e110c2f46ca083a58516343
exarkun <exarkun@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-10-06 19:33:03 +0000

    Change the way new links appear
    git-svn-id: http://divmod.org/svn/Divmod/trunk@476 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/static/js/live-clicks.js
+++ ClickChronicle/clickchronicle/static/js/live-clicks.js
@@ -1,14 +1,46 @@
 var clickchronicle_clickCount = 0;
 var clickchronicle_clickLimit = 10;
+
+function clickchronicle_setOpacity(node, ratio) {
+    node.style.filter = 'alpha(opacity=' + new String(ratio * 100) + ')'
+    node.style.opacity = ratio;
+}
+
+function clickchronicle_fadeIn(node, period) {
+    var ratio = 0.0;
+    function bump() {
+        if (ratio < 1) {
+            ratio += 0.2;
+            clickchronicle_setOpacity(node, ratio);
+            setTimeout(bump, 0.2);
+        }
+    }
+
+    setTimeout(bump, 0.05);
+}
+
+
+function clickchronicle_createClick(title, url) {
+    var newClick = document.createElement('div');
+    var clickLink = document.createElement('a');
+    var clickTitle = document.createTextNode(title);
+
+    clickLink.setAttribute('href', url);
+    clickLink.appendChild(clickTitle);
+    newClick.appendChild(clickLink)
+
+    clickchronicle_setOpacity(newClick, 0);
+
+    clickchronicle_fadeIn(newClick, 2);
+
+    return newClick;
+}
+
 function clickchronicle_addClick(title, url) {
     var clicks = document.getElementById('click-list');
     clickchronicle_clickCount += 1;
     if (clickchronicle_clickCount > clickchronicle_clickLimit) {
-        clicks.removeChild(clicks.firstChild);
+        clicks.removeChild(clicks.lastChild);
     }
-    var newClick = document.createElement('a');
-    newClick.setAttribute('href', url);
-    var clickTitle = document.createTextNode(title);
-    newClick.appendChild(clickTitle);
-    clicks.appendChild(newClick);
+    clicks.insertBefore(clickchronicle_createClick(title, url), clicks.firstChild);
 }

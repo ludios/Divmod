421d473ee51804d0063b4d71fcc90951109e929a
moe <moe@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-10-19 13:00:07 +0000

    simple module to create length-n markov chains from a given string
    git-svn-id: http://divmod.org/svn/Divmod/trunk@2471 866e43f7-fbfc-0310-8f2a-ec88d1da2979

---
+++ ClickChronicle/clickchronicle/stats/mchain.py
@@ -0,0 +1,30 @@
+adjacentNs = lambda s, n: (s[i:i+n] for i in xrange(len(s)-1))
+
+def mchain(string, tokenizer=str.split, length=1):
+    chains = dict()
+
+    def incCount(parent, word):
+        if word not in parent:
+            parent[word] = 0
+        parent[word] += 1
+
+    for portion in adjacentNs(tokenizer(string), length+1):
+        parent = chains
+        for (i, word) in enumerate(portion):
+            if i+1 == len(portion):
+                parent.setdefault(word, 0)
+                parent[word] += 1
+            else:
+                parent = parent.setdefault(word, dict())
+    return chains
+
+if __name__ == '__main__':
+
+    chains = mchain("""we face a lot of issues with gibberish in this world i think it probably has a lot
+                       to with the amount of noise words that you would find in a given piece of text
+                       lots of and and the and he and she and these are the kinds of problems that we face
+                       i cannot really see a likely way for us to over this soup of repetition, perhaps we
+                       are indeed doomed to failure""", length=3)
+
+    from pprint import pprint
+    pprint(chains)

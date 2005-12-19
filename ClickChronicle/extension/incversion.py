6a51b69a4fb55446874247023d9626bd3fba63e1
moe <moe@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2005-12-19 18:14:44 +0000

    add incversion script and RDF templates for update & install
    git-svn-id: http://divmod.org/svn/Divmod/trunk@3724 866e43f7-fbfc-0310-8f2a-ec88d1da2979

---
+++ ClickChronicle/extension/incversion.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from os import path
+
+def getversion(fname):
+    from xml.dom import minidom
+    doc = minidom.parse(fname)
+    versions = list(e.firstChild.nodeValue for e in
+                        doc.getElementsByTagNameNS(
+                            'http://www.mozilla.org/2004/em-rdf#', 'version'))
+    (version,) = set(versions)
+    return float(version)
+
+def main(newversion=None, files=('install.rdf', 'update.rdf')):
+    for fname in files:
+        if newversion is None:
+            curversion = getversion(fname)
+            newversion = curversion + 0.1
+        new = file(fname + '.tmpl').read() % dict(version=newversion)
+        file(fname, 'w').write(new)
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        newversion = argv[1]
+    elif len(argv) == 1:
+        newversion = None
+    else:
+        raise RuntimeError('expected zero or one arguments')
+
+    main(*argv[1:])

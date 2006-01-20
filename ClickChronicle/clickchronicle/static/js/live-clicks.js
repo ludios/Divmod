1950daf78410782f0762a17b92eac05dcd25d363
exarkun <exarkun@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2006-01-20 03:46:17 +0000

    Apply Widget.methods() patch from mithrandi, plus some more stuff
    This changes pretty much everything to use Widget.methods() 
    to define methods.  It also changes athenatest to use jsClass 
    most everyplace, and cleans up the "test harness" a bit.
    
    
    git-svn-id: http://divmod.org/svn/Divmod/trunk@4221 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/static/js/live-clicks.js
+++ ClickChronicle/clickchronicle/static/js/live-clicks.js
@@ -2,62 +2,61 @@ if(typeof(ClickChronicle) == "undefined") {
     ClickChronicle = {};
 }
 
-ClickChronicle.LiveClicks = Nevow.Athena.Widget.subclass();
+ClickChronicle.LiveClicks = Nevow.Athena.Widget.subclass('ClickChronicle.LiveClicks');
+ClickChronicle.LiveClicks.methods(
+    function loaded(self) {
+        self.clickCount = 0;
+        self.clickLimit = 10;
+        self.callRemote('getClickBacklog').addCallback(
+            function(clicks){ self.addClicks(clicks) });
+    },
 
-ClickChronicle.LiveClicks.prototype.loaded = function() {
-    this.clickCount = 0;
-    this.clickLimit = 10;
-    this.callRemote('getClickBacklog').addCallback(
-        MochiKit.Base.bind(function(clicks){ this.addClicks(clicks) }, this));
-}
+    function setOpacity(self, node, ratio) {
+        node.style.filter = 'alpha(opacity=' + new String(ratio * 100) + ')';
+        node.style.opacity = ratio;
+    },
 
-ClickChronicle.LiveClicks.prototype.setOpacity = function(node, ratio) {
-    node.style.filter = 'alpha(opacity=' + new String(ratio * 100) + ')';
-    node.style.opacity = ratio;
-}
-
-ClickChronicle.LiveClicks.prototype.fadeIn = function(node, period) {
-    var outerthis = this;
-    var ratio = 0.0;
-    function bump() {
-        if (ratio < 1) {
-            ratio += 0.2;
-            outerthis.setOpacity(node, ratio);
-            setTimeout(bump, 0.2);
+    function fadeIn(self, node, period) {
+        var ratio = 0.0;
+        function bump() {
+            if (ratio < 1) {
+                ratio += 0.2;
+                self.setOpacity(node, ratio);
+                setTimeout(bump, 0.2);
+            }
         }
-    }
-    setTimeout(bump, 0.05);
-}
+        setTimeout(bump, 0.05);
+    },
 
 
-ClickChronicle.LiveClicks.prototype.createClick = function(title, url) {
-    var newClick = MochiKit.DOM.DIV({'class': 'click_' + this.clickCount},
-                     MochiKit.DOM.A({'href': url}, title));
+    function createClick(self, title, url) {
+        var newClick = MochiKit.DOM.DIV({'class': 'click_' + self.clickCount},
+                         MochiKit.DOM.A({'href': url}, title));
 
-    this.setOpacity(newClick, 0);
-    this.fadeIn(newClick, 2);
+        self.setOpacity(newClick, 0);
+        self.fadeIn(newClick, 2);
 
-    return newClick;
-}
+        return newClick;
+    },
 
-ClickChronicle.LiveClicks.prototype.addClicks = function(clicks) {
-    for(var i = 0; i < clicks.length; i++) {
-        this.addClick.apply(this, clicks[i]);
-    }
-}
+    function addClicks(self, clicks) {
+        for(var i = 0; i < clicks.length; i++) {
+            self.addClick.apply(self, clicks[i]);
+        }
+    },
 
-ClickChronicle.LiveClicks.prototype.addClick = function(title, url) {
-    if(this.clickCount == 0) {
-        MochiKit.DOM.hideElement(
-            this.nodeByAttribute('class', 'big-title'));
-    }
+    function addClick(self, title, url) {
+        if(self.clickCount == 0) {
+            MochiKit.DOM.hideElement(
+                self.nodeByAttribute('class', 'big-title'));
+        }
 
-    this.clickCount++;
+        self.clickCount++;
 
-    if(this.clickLimit < this.clickCount) {
-        this.node.removeChild(this.nodeByAttribute(
-            'class', 'click_'+(this.clickCount-this.clickLimit)));
-    }
+        if(self.clickLimit < self.clickCount) {
+            self.node.removeChild(self.nodeByAttribute(
+                'class', 'click_'+(self.clickCount-self.clickLimit)));
+        }
 
-    this.node.insertBefore(this.createClick(title, url), this.node.firstChild);
-}
+        self.node.insertBefore(self.createClick(title, url), self.node.firstChild);
+    });

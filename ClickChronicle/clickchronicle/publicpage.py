ludios/Divmod

commit fdbfbeedecb9b03d5ca4022d100f8f49c1e152a0

Author: glyph Reviewer: moe
Fixes #382

This merge provides basic sharing functionality, such as the ability to give a
user a publicly-viewable web page (at /by/username, for the cool kids, or
/users, if you're a fuddy-duddy).

It also adds (working) support for a simple ClickChronicle public page at
/by/username/clicks, which exposes the "my clicks" page without the "actions"
column, and also (not working) Hyperbola at /by/username/blog which should some
day soon grow into a blog.  Patches accepted :).




git-svn-id: http://divmod.org/svn/Divmod/trunk@4021 866e43f7-fbfc-0310-8f2a-ec88d1da2979

ClickChronicle/clickchronicle/publicpage.py
@@ -300,11 +300,19 @@ class PublicIndexPage(CCPublicPageMixin, PublicAthenaLivePage):
 
     title = 'ClickChronicle'
     maxTitleLength = 70
-    maxClickQueryResults = 10
+    maxClickQueryResults = 0
 
     def __init__(self, publicPage, staticContent, forUser=None):
+        """
+        Create a PublicIndexPage, e.g. a resource to be displayed at the root
+        of the ClickChronicle application.
+
+        @param publicPage: a ClickChroniclePublicPage
+        @param staticContent: some nevow dom junk
+        @param forUser: a unicode user identifier
+        """
         templateContent = staticTemplate("index.html")
-        super(PublicIndexPage, self).__init__(None, None, templateContent, staticContent, forUser)
+        super(PublicIndexPage, self).__init__(templateContent, staticContent, forUser)
 
         self.clickContainerPattern = inevow.IQ(templateContent).patternGenerator('click-container')
 

ClickChronicle/clickchronicle/clickapp.py
@@ -21,7 +21,7 @@ from axiom.tags import Catalog
 
 from vertex import q2q
 
-from xmantissa import ixmantissa, webnav, website, webapp, prefs, search, tdbview
+from xmantissa import ixmantissa, webnav, website, webapp, prefs, search, tdbview, sharing
 
 from clickchronicle import iclickchronicle, indexinghelp, clickbrowser
 from clickchronicle.util import makeStaticURL
@@ -94,6 +94,15 @@ class _ShareClicks(prefs.MultipleChoicePreference):
                                            collection, desc,
                                            valueToDisplay)
 
+class _PublicPagePreference(prefs.MultipleChoicePreference):
+    def __init__(self, value, collection):
+        valueToDisplay = {True: "Yes", False: "No"}
+        desc = 'If set To "Yes", you will get a public page'
+        super(_PublicPagePreference, self).__init__('publicPage', value,
+                                                    'Give me a public page',
+                                                    collection, desc,
+                                                    valueToDisplay)
+
 class _TimezonePreference(prefs.Preference):
     def __init__(self, value, collection):
         super(_TimezonePreference, self).__init__('timezone', value, 'Timezone',
@@ -120,13 +129,37 @@ class CCPreferenceCollection(Item, InstallableMixin):
     timezone = attributes.text(default=u'US/Eastern')
     _cachedPrefs = attributes.inmemory()
 
+    def publicPage():
+        def get(self):
+            # figure out if we've shared this before...
+            try:
+                sharing.getShare(self.store,
+                                 sharing.getEveryoneRole(self.store),
+                                 shareID=u'clicks')
+                return True
+            except sharing.NoSuchShare:
+                return False
+
+        def set(self, value):
+            theClickList = list(self.store.query(ClickList))[0]
+            if value:
+                sharing.shareItem(
+                        theClickList,
+                        shareID=u'clicks',
+                        toRole=sharing.getEveryoneRole(self.store))
+            else:
+                sharing.unShare(theClickList)
+        return get, set
+    publicPage = property(*publicPage())
+
     def installOn(self, other):
         super(CCPreferenceCollection, self).installOn(other)
         other.powerUp(self, ixmantissa.IPreferenceCollection)
 
     def activate(self):
         self._cachedPrefs = {"shareClicks" : _ShareClicks(self.shareClicks, self),
-                             "timezone" : _TimezonePreference(self.timezone, self)}
+                             "timezone" : _TimezonePreference(self.timezone, self),
+                             "publicPage" : _PublicPagePreference(self.publicPage, self)}
 
     def getPreferences(self):
         return self._cachedPrefs
@@ -216,6 +249,13 @@ class ClickList(Item, InstallableMixin):
         """show a link to myself in the navbar"""
         return [webnav.Tab('Clicks', self.storeID, 0.9)]
 
+    allowedActions = (clickbrowser.bookmarkAction,
+         clickbrowser.ignoreVisitAction,
+         clickbrowser.privateVisitToggleAction,
+         clickbrowser.deleteAction)
+
+    sharing.allow('clicks', 'store')
+
 class DomainList(Item, InstallableMixin):
     """similar to Preferences, i am an implementor of INavigableElement,
        and PrivateApplication will find me when when it looks in the user's
@@ -257,12 +297,20 @@ class ClickListFragment(tdbview.TabularDataView):
         # will probably have to do something wacky in order to emulate
         # clickchronicle's method of displaying visit information in
         # the tdb
+        self.clickListItem = original
         (tdm, views) = clickbrowser.makeClickTDM(original.store, Visit)
-        tdbview.TabularDataView.__init__(self, tdm, views,
-                (clickbrowser.bookmarkAction,
-                 clickbrowser.ignoreVisitAction,
-                 clickbrowser.privateVisitToggleAction,
-                 clickbrowser.deleteAction))
+        allowedActions = self.getAllowedActions()
+        tdbview.TabularDataView.__init__(self, tdm, views, allowedActions)
+
+    def getAllowedActions(self):
+        return getattr(self.clickListItem, 'allowedActions', ())
+
+
+    def customizeFor(self, forUser):
+        # Hmm, this method probably shouldn't be required, but currently the
+        # sharing system invokes it unconditionally... remove me if that
+        # changes
+        return self
 
 registerAdapter(ClickListFragment,
                 ClickList,

ClickChronicle/clickchronicle/static/css/clickchronicle.css
@@ -72,7 +72,7 @@ a:hover   { color: #575757; text-decoration: none }
 a:active  { color: #FFFFFF; text-decoration: underline }
 
 .navRow { 
-    background-image: url(/static/mantissa/images/header_bg.png);
+    background-image: url(/Mantissa/images/header_bg.png);
     background-repeat: repeat-x;
     background-position: bottom;
     vertical-align: middle;
@@ -130,7 +130,7 @@ img.navIcon {
 
 tr.fancyRowFirst { 
     background-color: #FFFFFF;
-    background-image: url(/static/mantissa/images/header_bg.png);
+    background-image: url(/Mantissa/images/header_bg.png);
     background-repeat: repeat-x;
     background-position: bottom left 
 }
@@ -188,14 +188,14 @@ tr.fancyRowFirst {
 }
 
 td.sFancyCol:hover { 
-    background-image: url(/static/mantissa/images/header_bg_hover.png);
+    background-image: url(/Mantissa/images/header_bg_hover.png);
 }
 
 .lastFancyCol  { padding: 4px 4px 4px 4px; border-bottom: solid 1px #B0B0B0 }
 .sLastFancyCol { padding: 4px 4px 4px 4px; border-bottom: solid 1px #B0B0B0 }
 
 td.sLastFancyCol:hover {
-    background-image: url(/static/mantissa/images/header_bg_hover.png);
+    background-image: url(/Mantissa/images/header_bg_hover.png);
 }
 
 .linkIcon { border: 0; vertical-align: middle; }

ClickChronicle/clickchronicle/static/css/static-site.css
@@ -91,7 +91,7 @@ input.text {
 .click-set-collection > thead > tr > td {
     width: 50%;
     border: solid 1px #999999;
-    background-image: url(/static/mantissa/images/header_bg.png);
+    background-image: url(/Mantissa/images/header_bg.png);
     background-color: #FFFFFF;
     background-position: bottom left;
     background-repeat: repeat-x;

Divmod.pth
@@ -1,4 +1,4 @@
-# -*- test-case-name: "axiom -xclickchronicle -xepsilon -xxmantissa -xnevow -xformless -xpyflakes -xxquotient -xradical -xreverend -xsine -xvertex -xxapwrap" -*-
+# -*- test-case-name: "axiom -xclickchronicle -xepsilon -xxmantissa -xnevow -xformless -xpyflakes -xxquotient -xradical -xreverend -xsine -xvertex -xxapwrap -xhyperbola" -*-
 Axiom
 ClickChronicle
 Epsilon
@@ -11,3 +11,4 @@ Reverend
 Sine
 Vertex
 Xapwrap
+Hyperbola

Hyperbola/hyperbola/__init__.py
@@ -1,0 +1,5 @@
+# -*- test-case-name: hyperbola.test -*-
+# Don't put code here.
+from epsilon.versions import Version
+version = Version('hyperbola', 0, 0, 1)
+

Hyperbola/hyperbola/hyperblurb.py
@@ -1,0 +1,139 @@
+
+from epsilon.extime import Time
+
+from axiom.item import Item
+from axiom.attributes import text, reference, integer, timestamp
+
+from xmantissa.sharing import Role, allow
+
+class FLAVOR:
+    BLOG = u'FLAVOR.BLOG'
+    BLOG_POST = u'FLAVOR.BLOG_POST'
+
+    BLOG_COMMENT = u'FLAVOR.BLOG_COMMENT'
+
+    FORUM = u'FLAVOR.FORUM'
+    FORUM_TOPIC = u'FLAVOR.FORUM_TOPIC'
+    FORUM_POST = u'FLAVOR.FORUM_POST'
+
+    WIKI = u'FLAVOR.WIKI'
+    WIKI_NODE = u'FLAVOR.WIKI_NODE'
+
+    commentFlavors = {
+
+        BLOG: BLOG_POST,
+        BLOG_POST: BLOG_COMMENT,
+        BLOG_COMMENT: BLOG_COMMENT,
+
+        FORUM_POST: FORUM_POST,
+        FORUM_TOPIC: FORUM_POST,
+        FORUM: FORUM_TOPIC,
+
+        WIKI: WIKI_NODE,
+        WIKI_NODE: WIKI_NODE,
+
+        }
+
+    def ofCommentOn(cls, entry):
+        return cls.commentFlavors.get(entry)
+
+    ofCommentOn = classmethod(ofCommentOn)
+
+
+
+class MetaBlurb(Item):
+    """
+    I am an association of metadata with blurbs.
+
+    The difference between "core" metadata -- metadata which lives in L{Blurb}
+    -- and "extension" metadata -- metadata that lives in L{MetaBlurb} -- is
+    that MetaBlurb metadata should never be processed by Hyperbola itself.  It
+    is a mechanism which can be used for display purposes ("Mood", "Music",
+    "Current Obsession") or as a way to relay semi-structured information
+    ("Current Weather", "Relevance") to *other* software which will be
+    accessing Hyperbola publications as clients.
+    """
+
+    typeName = 'hyperbola_metablurb'
+    schemaVersion = 1
+
+    blurb = reference()
+
+    key = text()
+    value = text()
+
+
+class Blurb(Item):
+    """
+    I am some text written by a user.
+
+    'Blurb' is the super-generic term used to refer to all forms of publishing
+    and commentary within Hyperbola.  Traditionally entirely different software
+    packages have been constructed to handle 'forums', 'blogs', 'posts',
+    'comments', 'articles', etc.  In hyperbola, text written by a user in any
+    context becomes a blurb; whether it is displayed as forum software or blog
+    software would display it (and how: as a comment, as a topic, as a post) is
+    determined by its 'flavor'.
+
+    Flavors are described by L{FLAVOR}.
+    """
+
+    typeName = 'hyperbola_blurb'
+    schemaVersion = 1
+
+    dateCreated = timestamp()
+    dateLastEdited = timestamp()
+
+    title = text()
+    body = text()
+
+    hits = integer(doc="The number of times that this blurb has been displayed to users.",
+                   default=0)
+    author = reference(reftype=Role,
+                       allowNone=False)
+
+    parent = reference()        # to Blurb, but you can't spell that AGGUGHH
+    flavor = text(doc="One of FLAVOR's capitalized attributes.")
+
+    def edit(self, newTitle, newBody, newAuthor):
+        # Edit is only called on subsequent edits, not the first time, so we
+        # need to save our current contents as history.
+        editDate = Time()
+        PastBlurb(store=self.store,
+                  title=self.title,
+                  body=self.body,
+                  author=self.author,
+                  blurb=self,
+                  dateEdited=self.dateLastEdited,
+                  hits=self.hits)
+        self.title = newTitle
+        self.body = newBody
+        self.dateLastEdited = editDate
+        self.author = newAuthor
+
+
+    allow(edit,
+          'title', 'body', 'dateCreated', 'dateLastEdited', 'author',
+          'flavor', 'hits',
+          'parent')
+
+
+class PastBlurb(Item):
+    """
+    This is an old version of a blurb.  It contains the text as it used to be
+    at a particular point in time.
+    """
+
+    typeName = 'hyperbola_past_blurb'
+    schemaVersion = 1
+
+    dateEdited = timestamp()
+
+    title = text()
+    body = text()
+
+    hits = integer(doc="The number of times that this blurb has been displayed to users.")
+    author = reference(reftype=Role,
+                       allowNone=False)
+
+    blurb = reference(Blurb)

Hyperbola/hyperbola/hyperbola_model.py
@@ -1,0 +1,93 @@
+# -*- test-case-name: hyperbola.test.test_hyperbola -*-
+
+
+from zope.interface import implements
+
+from epsilon.extime import Time
+
+from axiom.item import Item, InstallableMixin
+from axiom.attributes import integer, reference
+
+from xmantissa import ixmantissa, website, webapp, webnav, tdb, sharing
+
+from twisted.python.components import registerAdapter
+
+from hyperbola.hyperbola_view import HyperbolaView
+from hyperbola.hyperblurb import Blurb
+
+class HyperbolaBenefactor(Item):
+    '''i am responsible for granting priveleges to avatars,
+       which equates to installing stuff in their store'''
+    implements(ixmantissa.IBenefactor)
+
+    typeName = 'hyperbola_benefactor'
+    schemaVersion = 1
+
+    # Number of users this benefactor has endowed
+    endowed = integer(default = 0)
+
+    def installOn(self, other):
+        other.powerUp(self, ixmantissa.IBenefactor)
+
+
+    def endow(self, ticket, avatar):
+        self.endowed += 1
+        # The user will be able to access the application using the
+        # HTTP Protocol
+        avatar.findOrCreate(website.WebSite).installOn(avatar)
+
+        # The user will be able to use private applications written for
+        # Mantissa and installed in his substore.
+        avatar.findOrCreate(webapp.PrivateApplication).installOn(avatar)
+
+        # Install this new application in the user substore (avatar) so that
+        # he will be able to use it.
+        avatar.findOrCreate(HyperbolaPublicPresence).installOn(avatar)
+
+
+    def deprive(self, ticket, avatar):
+        app = avatar.findFirst(HyperbolaPublicPresence)
+        app.deleteFromStore()
+
+
+
+class HyperbolaPublicPresence(Item, InstallableMixin):
+    # This object can be browsed from the web
+    implements(ixmantissa.INavigableElement)
+
+    schemaVersion = 1                  # First version of this object.
+    typeName = 'hyperbola_start'       # Database table name.
+
+    installedOn = reference()
+
+    def installOn(self, other):
+        super(HyperbolaPublicPresence, self).installOn(other)
+        other.powerUp(self, ixmantissa.INavigableElement)
+
+    def getTabs(self):
+        return [webnav.Tab('Hyperbola', self.storeID, 0)]
+
+    def getTDM(self):
+        """
+        Return a TabularDataModel listing all the blogs in this store.
+        """
+        return tdb.TabularDataModel(self.store, Blurb,
+                                    [Blurb.title, Blurb.dateCreated],
+                                    Blurb.parent == None)
+
+    def topPost(self, title, description, flavor):
+        now = Time()
+        return Blurb(
+            store=self.store,
+            dateCreated=now,
+            dateLastEdited=now,
+            title=title,
+            body=description,
+            flavor=flavor,
+            author=sharing.getSelfRole(self.store))
+
+# Notify the system that this Fragment class will be responsible of
+# rendering the model. The 'self.original' attribute of the HyperbolaView
+# instance is actually an instance of the HyperbolaPublicPresence class.
+registerAdapter(HyperbolaView, HyperbolaPublicPresence, ixmantissa.INavigableFragment)
+

commit e6ec2b8d8374f689a512804e64bc886237f8b6a3

err, "0" is not a good value to use as the limit of a query.  fixes CC public page (broken in r4021)
git-svn-id: http://divmod.org/svn/Divmod/trunk@4355 866e43f7-fbfc-0310-8f2a-ec88d1da2979

ClickChronicle/clickchronicle/publicpage.py
@@ -300,7 +300,7 @@ class PublicIndexPage(CCPublicPageMixin, PublicAthenaLivePage):
 
     title = 'ClickChronicle'
     maxTitleLength = 70
-    maxClickQueryResults = 0
+    maxClickQueryResults = 10
 
     def __init__(self, publicPage, staticContent, forUser=None):
         """
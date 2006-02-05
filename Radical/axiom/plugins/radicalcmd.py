34079ace46e00d44012a73297079f313917ff588
mithrandi <mithrandi@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2006-02-05 22:40:26 +0000

    Merge axiomatic-plugins-360.
    Closes #587
    Author: mithrandi
    Reviewer: glyph
    
    This adds two helper classes to axiom.scripts.axiomatic; AxiomaticCommand, and
    AxiomaticSubCommand. In order to create a new IAxiomaticCommand plugin, it is
    now sufficient to inherit from AxiomaticCommand. Sub commands should use
    AxiomaticSubCommand instead.
    
    The existing plugins are also converted to use these base classes.
    
    
    git-svn-id: http://divmod.org/svn/Divmod/trunk@4505 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- Radical/axiom/plugins/radicalcmd.py
+++ Radical/axiom/plugins/radicalcmd.py
@@ -1,19 +1,10 @@
 
-import os, sys
-
-from zope.interface import classProvides
-
-from twisted.python import usage
-from twisted import plugin
-
-from axiom import store, item, iaxiom, scheduler
-
-from xmantissa import signup
+from axiom import scheduler
+from axiom.scripts import axiomatic
 
 from radical import radapp
 
-class RadicalConfiguration(usage.Options):
-    classProvides(plugin.IPlugin, iaxiom.IAxiomaticCommand)
+class RadicalConfiguration(axiomatic.AxiomaticCommand):
 
     name = 'radical'
     description = 'omfg play the game now'

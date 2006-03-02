2699f892c40db55bc885e8fb1b91b73bc9afcefe
exarkun <exarkun@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2006-03-02 16:54:46 +0000

    revert indexer-subprocess-655-3
    git-svn-id: http://divmod.org/svn/Divmod/trunk@5133 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/test/historic/test_clickstats2to3.py
+++ ClickChronicle/clickchronicle/test/historic/test_clickstats2to3.py
@@ -5,17 +5,12 @@ from clickchronicle import publicpage
 from twisted.application.service import IService
 
 class ScoreMajig(stubloader.StubbedTest):
-    def setUp(self):
-        stubloader.StubbedTest.setUp(self)
-        self.service = IService(self.store)
-        self.service.startService()
-        return self.store.whenFullyUpgraded()
-
-
-    def tearDown(self):
-        return self.service.stopService()
-
-
     def testUpgrade(self):
-        cs = self.store.findUnique(publicpage.ClickStats)
-        self.failUnless(0  < cs.score, 'the score should be AT LEAST bigger than zero!!')
+        s = self.store
+        svc = IService(s)
+        svc.startService()
+        D = s.whenFullyUpgraded()
+        def _(fu):
+            cs = s.findUnique(publicpage.ClickStats)
+            self.failUnless(0  < cs.score, 'the score should be AT LEAST bigger than zero!!')
+        return D.addCallback(_)

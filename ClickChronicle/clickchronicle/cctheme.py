8cd68eba4f336dfdf403b92ec63de8a5cbb8240a
exarkun <exarkun@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2006-07-17 16:57:27 +0000

    Revert r7794 - Nevow/Python 2.3 regression
    git-svn-id: http://divmod.org/svn/Divmod/trunk@7796 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/cctheme.py
+++ ClickChronicle/clickchronicle/cctheme.py
@@ -2,13 +2,8 @@ from xmantissa import webtheme
 from nevow import tags
 
 class ClickChronicleTheme(webtheme.XHTMLDirectoryTheme):
-    def head(self, website):
-        root = website.cleartextRoot()
-        yield tags.link(
-            rel="stylesheet",
-            type="text/css",
-            href=root + "/ClickChronicle/static/css/clickchronicle.css")
-        yield tags.link(
-            rel="icon",
-            type="image/png",
-            href=root + "/ClickChronicle/static/images/favicon.png")
+    def head(self):
+        yield tags.link(rel="stylesheet", type="text/css",
+                        href="/ClickChronicle/static/css/clickchronicle.css")
+        yield tags.link(rel="icon", type="image/png",
+                        href="/ClickChronicle/static/images/favicon.png")

fe00ee0f42295d55440577ace10597265bed603d
exarkun <exarkun@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2006-07-17 16:48:16 +0000

    Merge most-cache-1130-2
    Author: exarkun
    Reviewer: moea
    Fixes #1130 and #1129
    
    Add support for document pre-processors to Nevow and exploit these
    in Mantissa to serve static content from very cacheable locations.
    
    
    git-svn-id: http://divmod.org/svn/Divmod/trunk@7794 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- ClickChronicle/clickchronicle/cctheme.py
+++ ClickChronicle/clickchronicle/cctheme.py
@@ -2,8 +2,13 @@ from xmantissa import webtheme
 from nevow import tags
 
 class ClickChronicleTheme(webtheme.XHTMLDirectoryTheme):
-    def head(self):
-        yield tags.link(rel="stylesheet", type="text/css",
-                        href="/ClickChronicle/static/css/clickchronicle.css")
-        yield tags.link(rel="icon", type="image/png",
-                        href="/ClickChronicle/static/images/favicon.png")
+    def head(self, website):
+        root = website.cleartextRoot()
+        yield tags.link(
+            rel="stylesheet",
+            type="text/css",
+            href=root + "/ClickChronicle/static/css/clickchronicle.css")
+        yield tags.link(
+            rel="icon",
+            type="image/png",
+            href=root + "/ClickChronicle/static/images/favicon.png")

caf6075c81fa239143e5014ece848921888e4cfa
glyph <glyph@866e43f7-fbfc-0310-8f2a-ec88d1da2979>
2009-03-28 19:35:36 +0000

    Remove distutils-mangling code from Combinator which breaks setuptools.
    After this change, Combinator will no longer attempt to force 'python setup.py install' to put things into your home directory.  Use `setup.py --prefix ~/.local`, or, if your package is trying to use setuptools, `python setup.py --site-dirs ~/.local/lib/python2.5/site-packages --prefix ~/.local install`.
    
    Author: glyph
    
    Reviewer: dried
    
    Fixes #493
    
    
    git-svn-id: http://divmod.org/svn/Divmod/trunk@17234 866e43f7-fbfc-0310-8f2a-ec88d1da2979

--- Combinator/sitecustomize.py
+++ Combinator/sitecustomize.py
@@ -18,25 +18,3 @@ for key in sys.modules.keys():
     if key == 'combinator' or key.startswith('combinator'):
         del sys.modules[key]
 
-# Install stuff as a user, by default.
-
-if sys.platform != 'darwin':
-    # For use with setup.py...
-
-    if sys.platform.startswith('win'):
-        execprefix = os.path.abspath(os.path.expanduser("~/Python"))
-    else:
-        # Don't exactly know how Darwin fits in here - I think distutils is
-        # buggy...?
-        execprefix = os.path.abspath(os.path.expanduser("~/.local"))
-
-    import sys
-
-    class DistSysProxy:
-        def __getattr__(self, attr):
-            if attr in ('prefix', 'exec_prefix'):
-                return execprefix
-            else:
-                return getattr(sys, attr)
-
-    sys.modules['distutils.command.sys'] = DistSysProxy()
